c4fc5523c8e251fcd30619f950d0a4a0072f23fe
Egor Zyazev <klever.kl@yandex.ru>
2018-04-16 20:06:56 +0300

    trapzd static variable bugfix

--- count.c
+++ count.c
@@ -3,11 +3,9 @@
 #include <pthread.h>
 
 
-double trapzd(double (*func)(double), double a, double b, int n) {
+double trapzd(double (*func)(double), double a, double b, int n, double s) {
     double x,tnm,sum,del;
-    static double s;
     int it,j;
-
     if (n == 1) {
         return (s=0.5*(b-a)*(func(a)+func(b)));
     } else {
@@ -17,24 +15,25 @@ double trapzd(double (*func)(double), double a, double b, int n) {
         x=a+0.5*del;
         for (sum=0.0,j=1;j<=it;j++,x+=del) sum += func(x);
         s=0.5*(s+(b-a)*sum/tnm);
-        printf("TRAPZD: s: %f it: %d del:%f/n", s, it, del);
+        //printf("         %d: TRAPZD: s: %f it: %d del:%f\n",pthread_self(), s, it, del);
         return s;
     }
 }
 
 double qsimp(double (*func)(double), double a, double b) {
     int j;
-    double s,st,ost=0.0,os=0.0;
+    double s,st,ost,os;
+    ost=os=0;
     printf("LOL %.3e %.3e\n",a,b);
     for (j=1;j<=JMAX;j++) {
-        st=trapzd(func,a,b,j);
-        //printf("%.3e\n",st);
+        st=trapzd(func,a,b,j, s);
+        printf("%d: %.3e %.3e\n",pthread_self(), st, ost);
         s=(4.0*st-ost)/3.0;
-        printf("%d: CHECK %d s: %f os: %f | %f %f\n", pthread_self(), j, s, os, fabs(s - os), EPS * fabs(os));
+        //printf("%d: CHECK %d s: %f os: %f | %f %f\n", pthread_self(), j, s, os, fabs(s - os), EPS * fabs(os));
         if (j > 5) {
             if (fabs(s - os) <= EPS * fabs(os) ||
                 (s == 0.0 && os == 0.0)) {
-                printf("qsimp took %d steps to reach eps\n", j);
+                //printf("qsimp took %d steps to reach eps\n", j);
                 return s;
             }
         }
@@ -42,22 +41,6 @@ double qsimp(double (*func)(double), double a, double b) {
         ost=st;
 
     }
-    printf("\"%d: EPSILON %f %f %e\n",pthread_self(), s, os, fabs(s-os));
+    //printf("---%d: EPSILON %f %f %e\n",pthread_self(), s, os, fabs(s-os));
     return s;
-}
-
-double qtrap(double (*func)(double), double a, double b) {
-    int j;
-    double s,olds=0.0;
-
-    for (j=1;j<=JMAX;j++) {
-        s=trapzd(func,a,b,j);
-        if (j > 5)
-            if (fabs(s-olds) <= EPS*fabs(olds) || (s == 0.0 && olds == 0.0)) {
-                printf("qtrap took %d steps to reach eps\n", j);
-                return s;
-            }
-        olds=s;
-    }
-    return 0.0;
 }
