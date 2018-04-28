e0fa55269cd35da16756eb15ab1cde6840e8e872
Egor Zyazev <klever.kl@yandex.ru>
2018-04-28 07:00:43 +0300

    same as previous, but clean

--- main.c
+++ main.c
@@ -120,7 +120,6 @@ int main(int argc, char *argv[]) {
     memset(queue, -1, n * sizeof(int));
 
     while ((res = fscanf(cpuinfo_file, "processor : %d\ncore id : %d\n", &processor, &coreId)) == 2) {
-        printf("processor : %d\ncore id : %d\n", processor, coreId);
         procs[processor].aff = coreId;
         if(procs[processor].loaded) cores[coreId].load++; //we have main thread on cpu
 
@@ -133,11 +132,9 @@ int main(int argc, char *argv[]) {
             bo[i].b = a + (b - a) / n * (i + 1);
 
             if (processor == sched_getcpu()) {//works with first
-                if (processor == mainP || procs[mainP].loaded) { //the process was taken by main from the begining
+                if (processor == mainP || procs[mainP].loaded) { //the process was taken by main from the beginning
                     if(procs[mainP].loaded==0) cores[coreId].load++;
                     procs[mainP].loaded = 1; //main taken a thread
-
-                    printf("THREAD STORED IN QUEUE BY MAIN\n");
                     queue[s] = i;
                     s++;
                 }
@@ -151,11 +148,6 @@ int main(int argc, char *argv[]) {
                     }
                     if(procs[mainP].loaded==0) cores[procs[mainP].aff].load++;
                     procs[mainP].loaded = 1;//setting thread to the old spot of the main
-                    //procs[mainP].loaded = 0; //main freed a thread
-                    //cores[procs[mainP].aff].load--;
-                    //if(!threads[i]) threads[i]=1; //some kind of bug: returns 0 thread id
-                    printf("%lu THREAD STARTED IN CYCLE MAIN mainP: %d processor: %d\n", threads[i], mainP, processor);
-                    //mainP = sched_getcpu(); //getting new location for main
                     if(procs[processor].loaded==0) cores[procs[processor].aff].load++;
                     procs[processor].loaded = 1; //main taken new thread
 
@@ -173,36 +165,13 @@ int main(int argc, char *argv[]) {
                 }
                 procs[processor].loaded = 1;
                 cores[coreId].load++;
-                //if(!threads[i]) threads[i]=1; //some kind of bug: returns 0 thread id
-                printf("%lu load: %d THREAD STARTED IN CYCLE %d\n",threads[i], cores[coreId].load, processor);
-
             } else { //storing in queue
-                printf("THREAD STORED IN QUEUE\n");
-
                 queue[s] = i;
                 s++;
             }
             i++;
         } else if (s > 0) { //running stored in queue
             printf("%d\n", s);
-            /*if (processor == sched_getcpu()) {//main thread changed its location
-                CPU_ZERO(&mask);
-                CPU_SET(mainP, &mask); //sending thread to the last main location
-                pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &mask);
-                if (pthread_create(&threads[s], &attr, threadFunc, &bo[s])) {//sending sth from the queue
-                    printf("err creating thread");
-                    return 0;
-                }
-                procs[processor].loaded = 1;//exchange current for main, load is the same for both
-                cores[coreId].load++;
-                mainP = sched_getcpu();
-                queue[s] = -1;
-                s--;
-                if(!threads[i]) threads[i]=1; //some kind of bug: returns 0 thread id
-                printf("%lu THREAD STARTED IN QUEUE CYCLE MAIN %d\n",threads[s], processor);
-                continue;
-
-            }*/
             if (cores[coreId].load < procsNum / 2 && processor != sched_getcpu()) {
                 CPU_ZERO(&mask);
                 CPU_SET(processor, &mask);
@@ -212,10 +181,8 @@ int main(int argc, char *argv[]) {
                     return 0;
                 }
                 procs[processor].loaded = 1;
-                //if(!threads[i]) threads[i]=1; //some kind of bug: returns 0 thread id
                 cores[coreId].load++;
                 queue[s-1] = -1;
-                printf("%lu THREAD STARTED IN QUEUE CYCLE %d\n",threads[queue[s-1]], processor);
                 s--;
             }
         }
@@ -237,11 +204,8 @@ int main(int argc, char *argv[]) {
         if (!cores[q].procs[0]) continue; //no core with this id
         if (cores[q].load == 2) continue; //all core is taken
         coreId = q;
-        //processor = cores[q].procs[cores[q].load][s].id;
         if (!cores[q].procs[0]->loaded) processor = cores[q].procs[0]->id;
         else processor = cores[q].procs[1]->id;
-        printf("%d processor: %d cpu1loaded?: %d cpu2loaded?: %d\n", cores[q].load, processor,
-               procs[cores[q].procs[0]->id].loaded, procs[cores[q].procs[1]->id].loaded);
         if (processor == sched_getcpu()) {//main thread changed its location
             if(procs[mainP].loaded)cores[procs[mainP].aff].load++;
             procs[mainP].loaded=0;
@@ -249,20 +213,6 @@ int main(int argc, char *argv[]) {
             procs[processor].loaded=1;
             mainP=sched_getcpu();
         } else {
-            /*CPU_ZERO(&mask);
-            CPU_SET(mainP, &mask); //sending thread to the last main location
-            pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &mask);
-            if ((pthread_create(&threads[s], &attr, threadFunc, &bo[s])) != 0) {
-                printf("err creating thread");
-                return 0;
-            }
-            printf("THREAD STARTED IN QUEUE MAIN %d\n", mainP);
-            procs[processor].loaded = 1;//exchange current for main, load is the same for both
-            cores[coreId].load++;
-            mainP = processor;
-            s--;
-            continue;*/
-
             CPU_ZERO(&mask);
             CPU_SET(processor, &mask);
             pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &mask);
@@ -274,42 +224,17 @@ int main(int argc, char *argv[]) {
             procs[processor].loaded = 1;
             cores[coreId].load++;
             s--;
-            printf("THREAD STARTED IN QUEUE %d\n", processor);
         }
         if (q == coreIdMax && s > 0) q = -1;
     }
-
-
     bo[n - 1].a = a + (b - a) / n * (n - 1);
     bo[n - 1].b = a + (b - a) / n * (n);
-    struct timespec t;
-    t.tv_nsec = 100;
-    t.tv_sec = 0;
     result = result + *((double *) threadFunc(&bo[n - 1]));
     double *ret[n];
-    int k = n-1;
-    int d;
-    /*for (int r = 0; r <= n && k>0; r++) {
-        //if(!threads[i]) continue;
-        if (threads[r]) {
-            if (!(d = pthread_timedjoin_np(threads[r], (void **) &ret[r], &t))) {
-                t.tv_nsec = 10000;
-                t.tv_sec = 0;
-            }
-            if (d == 0) {
-                result += *ret[r];
-                threads[r] = NULL;
-                k--;
-            }
-        }
-        if (r == n && k > 0)
-            r = -1;
-            */
     for(int r=0; r<n && n>1; r++){//nonblocking join makes no sense
-        //if(!threads[r]) continue;
         pthread_join(threads[r], (void**) &ret[r]);
         result+=*ret[r];
-    } /* Wait until thread is finished */
+    } /* Wait until threads are finished */
     printf("%e\n", result);
     return 0;
 }
